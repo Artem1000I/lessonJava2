6a38e9651a455ea53c2f019534a14d5bcb111e79
Artem <93914638+Artem1000I@users.noreply.github.com>
2022-09-05 22:37:43 +1000

    Update HomeWork_2.java

--- src/main/java/org/example/HomeWork_2.java
+++ src/main/java/org/example/HomeWork_2.java
@@ -14,9 +14,7 @@ public class HomeWork_2 {
          System.out.println(year(2022));
 
 
-
-
-    }
+}
     //Написать метод, принимающий на вход два целых числа и проверяющий,
     // что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false
     public static boolean numbers (int a, int b){
